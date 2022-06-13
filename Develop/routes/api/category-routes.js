a760f25aa191e93dc9fe393c4efc793eb7c85217
hkagei <k_dawg_1022@hotmail.com>
2022-06-12 19:17:01 -0600

    hw

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,10 +5,32 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}]
+    });
+    
+    if (!categoryData) {
+      res.status(404).json({ message: 'No product found!'});
+      return;
+    }
+    
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
