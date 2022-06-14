9a8b3ff820e73a27e7f1257b9b512d99df4f8e9e
hkagei <k_dawg_1022@hotmail.com>
2022-06-13 18:48:36 -0600

    fix code syntax in api

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,32 +5,29 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  try {
-    const categoryData = await Category.findAll({
+Category.findAll({
       include: [{ model: Product }]
-    });
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-  // be sure to include its associated Products
+    })
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(400).json(err))
 });
+    
+  // be sure to include its associated Products
 
 router.get('/:id', (req, res) => {
-  try {
-    const categoryData = await Category.findByPk(req.params.id, {
+
+  Category.findByPk(req.params.id, {
       include: [{ model: Product}]
-    });
-    
-    if (!categoryData) {
-      res.status(404).json({ message: 'No product found!'});
-      return;
+    })
+   .then((categories) => res.json(categories))
+   .catch((err) => res.status(400).json(err))
+   
+  if (!categoryData) {
+  res.status(404).json({ message: 'No product found!'});
+    return;
     }
     
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    
   // find one category by its `id` value
   // be sure to include its associated Products
 });
@@ -47,6 +44,12 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+    Category.update(
+      {
+        category_name: req.body.category_name,
+
+      },
+    )
 });
 
 router.delete('/:id', (req, res) => {
