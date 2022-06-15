2f0653d38816a66b7e5dc7bb5def55a4b5cde4f5
hkagei <k_dawg_1022@hotmail.com>
2022-06-14 23:35:04 -0600

    write out index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,18 +7,20 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   // (through goes here)
-  
-
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  through: '',
+  foreignKey: '',
 
 });
 
